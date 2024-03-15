248d473bc454382d8f94ab1968e0ca2b6e5c916a
stephen-nene <stevekid705@gmail.com>
2024-03-15 14:26:07 +0300

    new structure

---
+++ rails-end/README.md
@@ -0,0 +1,6 @@
+# Rails-End
+
+```sh
+bundle config set path 'vendor/bundle'
+bundle install --path ~/.gem
+```
---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# fuser -k 1420/tcp
+# Start the Rails server
+# cd backend
+# rails s &
+
+# Wait for the Rails server to start (adjust this sleep duration based on your app's startup time)
+# sleep 5
+
+# Start the Tauri app
+cd client
+npm run dev -- --host
+
+# If you want to stop the Rails server when the Tauri app exits, you can uncomment the line below
+pkill -f "rails s"
+
+
+# if -d run tauri if -w run dev 
