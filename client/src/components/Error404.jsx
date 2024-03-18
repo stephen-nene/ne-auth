45933d1cd31f30e47a521857478e5e7784ff2f89
stephen-nene <stevekid705@gmail.com>
2024-03-18 09:33:50 +0300

    data sent

--- client/src/components/Error404.jsx
+++ client/src/components/Error404.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Error404() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-5xl font-bold text-gray-800 mb-4">Oops! Page Not Found</h1>
+            <h1 className="text-5xl font-bold text-gray-900 mb-4">Oops! Page Not Found</h1>
             <p className="text-xl text-gray-600 mb-8">The page you are looking for might have been removed or its name changed, or it's temporarily unavailable.</p>
             <Link to="/" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Go to Home</Link>
         </div>
--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -8,6 +8,11 @@ import leaf from '../../assets/images/tech1.jpeg';
 export default function Login({ darkMode }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [rememberMe, setRememberMe] = useState(false);
+
+    const handleRememberMeChange = () => {
+        setRememberMe((prevRememberMe) => !prevRememberMe);
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,14 +23,16 @@ export default function Login({ darkMode }) {
             return;
         }
 
-        // Log in logic goes here
-        console.log("Logging in with:", { email, password });
+        // Log in the user
+        console.log("Login credentials:", { email, password, rememberMe });
     };
 
     return (
         <div className="flex justify-center h-screen">
+            {/* Left Side */}
             <div className={`hidden lg:flex w-full lg:w-1/2 items-center justify-center ${darkMode ? 'bg-gray-100' : 'bg-gray-700'}`}>
-                <div className="p-6 rounded-md ">
+                {/* Left Side Content */}
+                <div className="p-6 rounded-md">
                     <h1 className="text-4xl font-bold">Welcome back to</h1>
                     <span className="text-6xl font-semibold text-white transform -translate-y-1/2 mb-2">
                         <span className="text-orange-600">Eco</span>
@@ -36,32 +43,39 @@ export default function Login({ darkMode }) {
                 </div>
             </div>
 
+            {/* Right Side */}
             <div className={`flex flex-col justify-center w-full lg:w-1/2  ${darkMode ? 'bg-gray-300' : 'bg-gray-800'} p-8`}>
                 <h1 className="text-3xl font-bold mb-4">Sign in to your account</h1>
                 <p className={`text-gray-600 ${darkMode ? 'text-gray-400' : ''} mb-8`}>
                     Continue on your path in our platform.
                 </p>
 
+                {/* Login Form */}
                 <form className="grid gap-4" onSubmit={handleSubmit}>
                     <input className={`border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500`} type="text" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     <input className={`border border-gray-300  rounded-md py-2 px-3 focus:outline-none focus:border-blue-500`} type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
+                    {/* Remember Me Checkbox */}
                     <div className="flex items-center justify-between">
                         <label className="flex items-center text-sm">
-                            <input type="checkbox" className="mr-2" />
+                            <input type="checkbox" className="mr-2" checked={rememberMe} onChange={handleRememberMeChange} />
                             <span className={`${darkMode ? 'text-gray-800' : 'text-gray-300'}`}>Remember me</span>
                         </label>
                         <Link to="/forgot-password" className={`text-blue-500 ${darkMode ? 'text-blue-400' : ''}`}>Forgot password?</Link>
                     </div>
 
+                    {/* Login Button */}
                     <button className={`bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50`}>
                         Sign in
                     </button>
                 </form>
 
+                {/* Not Registered */}
                 <div className="my-7">
                     <p className={` ${darkMode ? 'text-gray-700' : 'text-gray-200'} `}>Not registered? <Link to="/register" className={`text-blue-500 ${darkMode ? 'dark:text-blue-400' : ''}`}>Create an account</Link></p>
                 </div>
+
+                {/* Continue with Social Media */}
                 <div className="mt-8 text-center">
                     <hr className={`w-3/4 mx-auto border-t border-gray-950 ${darkMode ? 'text-gray-800' : 'text-gray-300'} opacity-50`} />
                     <p className={`text-sm text-gray-500 ${darkMode ? 'text-gray-700' : 'text-gray-300'} relative inline-block px-4  ${darkMode ? 'bg-gray-300' : 'bg-gray-800'} -top-[14px]`}>Or continue with</p>
@@ -69,7 +83,6 @@ export default function Login({ darkMode }) {
                         <button className={`rounded-full bg-white hover:bg-gray-900 text-white p-3 ${darkMode ? ' text-gray-900' : ''}`}>
                             <FcGoogle onClick={() => message.info('Coming soon')} />
                         </button>
-
                         <button className={`rounded-full bg-blue-500 hover:bg-blue-700 text-white ml-4 p-3 ${darkMode ? ' text-gray-900' : ''}`}>
                             <FaFacebookF onClick={() => message.info('Coming soon')} />
                         </button>
