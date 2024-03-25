1d8c0b1ad852a27dff0e5da9c6c5df6764ea63bd
stephen-nene <stevekid705@gmail.com>
2024-03-25 14:36:48 +0300

    shop game

--- client/src/components/pages/Shop.jsx
+++ client/src/components/pages/Shop.jsx
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function Shop() {
     return (
-        <div className="flex  items-cente justify-cente h-screen">
-<p>sss</p>
+        <div className="mt-[100px] p-5">
+
+        <div className=" flex  items-cente justify-cente h-screen">
+            <p>sss</p>
             <h1 className="text-5xl font-bold text-gray-900 mb-4">Shop</h1>
+        </div>
         </div>
     );
 }
+``
