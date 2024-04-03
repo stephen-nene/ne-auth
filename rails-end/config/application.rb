54a1aa4a5fd133cdf6540943b83300eea62132a1
stephen-nene <stevekid705@gmail.com>
2024-04-03 10:23:13 +0300

    can signu too

--- rails-end/config/application.rb
+++ rails-end/config/application.rb
@@ -12,7 +12,9 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
+require "dotenv/load"
 # require "rails/test_unit/railtie"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
